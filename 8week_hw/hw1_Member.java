5cd7e5361de177e7045d3481bee63338cd28d5e2
민현기 <56817525+bluesky0990@users.noreply.github.com>
2020-06-03 16:27:57 +0900

    Add files via upload

---
+++ 8week_hw/hw1_Member.java
@@ -0,0 +1,34 @@
+package ch10;
+
+public class hw1_Member {
+	private String stNum;
+	private String name;
+	private String grade;
+	private String subject;
+	
+	
+	public String getStNum() {
+		return stNum;
+	}
+	public void setStNum(String stNum) {
+		this.stNum = stNum;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+}
